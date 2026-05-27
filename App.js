022f04c1b16711574fbe5caad884c48e32fc7ebf
Riyad Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-05-27 11:41:07 +0200

    :sparkles: add two screen for taquin and informations

--- App.js
+++ App.js
@@ -1,5 +1,27 @@
 import Taquin from "./src/taquin";
+import { createStaticNavigation } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Score from "./src/score";
+import About from "./src/about";
+
+const MyTabs = createMaterialTopTabNavigator({
+  screens: {
+    Score: Score,
+    About: About,
+  },
+});
+
+const RootStack = createNativeStackNavigator({
+  initialRouteName: "MyTabs",
+  screens: {
+    Taquin: Taquin,
+    MyTabs,
+  },
+});
+
+const Navigation = createStaticNavigation(RootStack);
 
 export default function App() {
-  return <Taquin />;
+  return <Navigation />;
 }
