6b9dfa540ecc7d296e26614a7ce49f2b951f53a3
Riyad Zerrougui <riyad.zerrougui@etudiant.univ-reims.fr>
2026-05-27 10:02:31 +0200

    :sparkles: add possibility to choose an image from gallery for taquin

--- src/pictureSelector.js
+++ src/pictureSelector.js
@@ -1,4 +1,10 @@
-import { StyleSheet, Button } from "react-native";
-export default function PictureSelector() {
-  return <Button title={"Picture Selector"} />;
+import { Button } from "react-native";
+import PropTypes from "prop-types";
+
+export default function PictureSelector({ onPress }) {
+  return <Button title={"Picture Selector"} onPress={onPress} />;
 }
+
+PictureSelector.PropTypes = {
+  onPress: PropTypes.func.isRequired,
+};
--- src/taquin.js
+++ src/taquin.js
@@ -1,13 +1,39 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
 import Title from "./title";
 import TileGrid from "./tileGrid";
 import PictureSelector from "./pictureSelector";
 import Footer from "./footer";
 import { useState } from "react";
+import * as ImagePicker from "expo-image-picker";
 
 export default function Taquin() {
   const [minDimension, setMinDimension] = useState(100);
+  const [imageUri, setImageUri] = useState(null);
+
+  const pickImage = async () => {
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
+
+    if (!permissionResult.granted) {
+      Alert.alert(
+        "Permission required",
+        "Permission to access the media library is required.",
+      );
+      return;
+    }
+
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ["images", "videos"],
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log("iciiiii", { uri: result["assets"][0]["uri"] });
+
+    setImageUri(result["assets"][0]["uri"]);
+  };
+
   return (
     <View
       style={styles.container}
@@ -22,13 +48,8 @@ export default function Taquin() {
     >
       <StatusBar style="light" />
       <Title />
-      <TileGrid
-        dimension={minDimension}
-        sourcePicture={
-          "https://www.freedigitalphotos.net/images/img/homepage/87357.jpg"
-        }
-      />
-      <PictureSelector />
+      <TileGrid dimension={minDimension} sourcePicture={imageUri} />
+      <PictureSelector onPress={pickImage} />
       <Footer />
     </View>
   );
